b4b731f84d5a3eb21680e32b39bcd763db8cd121
Jay Hemsley <jay@hemsley.dev>
2024-05-06 20:34:51 -0400

    correct flex issue when zoomed out

--- src/ui/components/hero/hero.tsx
+++ src/ui/components/hero/hero.tsx
@@ -8,8 +8,8 @@ export function Hero({
   subtitle?: string;
 }): React.ReactElement {
   return (
-    <section className="py-16 sm:py-20 md:py-24 lg:py-28 xl:py-32 2xl:py-40">
-      <div className="container flex justify-center">
+    <section className="w-full py-16 sm:py-20 md:py-24 lg:py-28 xl:py-32 2xl:py-40">
+      <div className="container mx-auto flex justify-center">
         <div className="flex w-11/12 flex-wrap gap-2 sm:w-10/12 sm:gap-4">
           {title && (
             <h1 className="bg-gradient-to-br from-secondary-darkest to-secondary-light bg-clip-text text-4xl/[1.1] font-black text-transparent sm:text-5xl/[1.1] md:text-6xl/[1.1] lg:text-7xl/[1.1] xl:text-8xl/[1.1] dark:from-secondary-light dark:to-white">
